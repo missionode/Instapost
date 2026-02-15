5232106e52c1bc719b9f73ad54a8ae1b3a7ebf47
bodhiintellect <nath.syam.1986@gmail.com>
2026-02-15 21:29:38 +0530

    feat(ui): Implement the dynamic form UI with all required fields

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 describe('index.html structure', () => {
   let html;
@@ -22,5 +25,51 @@ describe('index.html structure', () => {
 
   test('should have a title tag with Instapost', () => {
     expect(html).toMatch(/<title>Instapost<\/title>/i);
+  });
+});
+
+describe('index.html form elements', () => {
+  const { JSDOM } = require('jsdom');
+  let dom;
+  let document;
+
+  beforeAll(() => {
+    const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+    dom = new JSDOM(html);
+    document = dom.window.document;
+  });
+
+  test('should have a form with id "instapost-form"', () => {
+    expect(document.getElementById('instapost-form')).toBeTruthy();
+  });
+
+  test('should have a design_type input/select', () => {
+    expect(document.querySelector('[name="design_type"]')).toBeTruthy();
+  });
+
+  test('should have a dress_reference input', () => {
+    expect(document.querySelector('[name="dress_reference"]')).toBeTruthy();
+  });
+
+  test('should have content fields: hook, brand, event_offer, location_details, contact_details, social_handles', () => {
+    expect(document.querySelector('[name="hook"]')).toBeTruthy();
+    expect(document.querySelector('[name="brand"]')).toBeTruthy();
+    expect(document.querySelector('[name="event_offer"]')).toBeTruthy();
+    expect(document.querySelector('[name="location_details"]')).toBeTruthy();
+    expect(document.querySelector('[name="contact_details"]')).toBeTruthy();
+    expect(document.querySelector('[name="social_handles"]')).toBeTruthy();
+  });
+
+  test('should have a "Generate Prompt" button', () => {
+    expect(document.getElementById('generate-btn')).toBeTruthy();
+  });
+
+  test('should have an output area for the prompt', () => {
+    expect(document.getElementById('prompt-output')).toBeTruthy();
+  });
+
+  test('should have data management buttons: download and upload', () => {
+    expect(document.getElementById('download-db-btn')).toBeTruthy();
+    expect(document.getElementById('upload-db-btn')).toBeTruthy();
   });
 });
